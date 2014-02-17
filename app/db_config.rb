7b8ee3e7cdbbad9fcbfc7556a44b752b22738f65
Berta-G <berta.gutierrez@gmail.com>
2014-02-17 13:52:29 +0000

    Fixed db_config url

--- app/db_config.rb
+++ app/db_config.rb
@@ -1,7 +1,7 @@
 env = ENV['RACK_ENV'] || 'development'
 
 # DataMapper.setup(:default, "postgres://localhost/chitter_#{env}")
-DataMapper.setup(:default, DATABASE_URL)
+DataMapper.setup(:default, ENV['DATABASE_URL'])
 
 Dir.glob(File.join(File.dirname(__FILE__), 'models', '*.rb'), &method(:require))
 
